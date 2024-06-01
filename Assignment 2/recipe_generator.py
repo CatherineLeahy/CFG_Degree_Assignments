abf90cd0bf18a5115a93e40632f1504e528dac8f
Catherine Leahy <catleahy9@msn.com>
2024-06-01 21:13:43 +0100

    requests added to requirements.txt and further updates to assig 2

--- Assignment 2/recipe_generator.py
+++ Assignment 2/recipe_generator.py
@@ -1,5 +1,20 @@
+import pprint
 
-def ingredients_is_valid(location):
+import requests
+
+API_ID = ''
+API_KEY = ''
+
+
+def get_recipe(ingredients, api_id, api_key):
+    url = 'https://api.edamam.com/api/recipes/v2?type=public&q={}&app_id={}&app_key=%20{}'.format(ingredients, api_id, api_key)
+    response = requests.get(url)
+    data = response.json()
+    single_recipe = data['hits'][0]['recipe']
+    return single_recipe
+
+
+def ingredients_is_valid(ingredients):
     return True
 
 
@@ -10,10 +25,13 @@ def ask_ingredients():
     else:
         return ask_ingredients()
 
-def main():
-    print("Hello! Allow Edamam's API to decide what's for dinner! Tell us what's in your fridge, and we'll provide the recipe. Bon apetite!.")
-    ingredients = ask_ingredients()
 
+def main():
+    print("Hello! Allow Edamam's API to decide what's for dinner! Tell us what's in your fridge, and we'll provide the"
+          " recipe. Bon apetite!.")
+    ingredients = ask_ingredients()
+    recipe = get_recipe(ingredients, API_ID, API_KEY)
+    pprint.pprint(recipe)
 
 if __name__ == "__main__":
     main()
