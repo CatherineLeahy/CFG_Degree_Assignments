d6d7977409d165a463e04d3d7bdff25e2e035e34
Catherine Leahy <catleahy9@msn.com>
2024-06-02 20:35:58 +0100

    comments added to code

--- Assignment 2/recipe_generator.py
+++ Assignment 2/recipe_generator.py
@@ -1,62 +1,72 @@
-# Imported modules, these are stored in the requirements.txt file
+# additional module imported (used to capture the time it takes the API to return the recipe)
 from datetime import datetime
-import pprint
+
 import requests
 
 
-# My API access ID and key, see README for instructions on acquiring your own ID and key.
+# my API access ID and key, see README for instructions on acquiring your own ID and key.
 API_ID = 'ae7b3fe0'
 API_KEY = '9df3c1657ee8917acb0978d30b1af0e6'
 
 
-# Method for
+# method for passing user inputs to API and returning a recipe
 def get_recipe(ingredients, api_id, api_key):
+    # if no ingredients are inputted by user, a random recipe is called from the API
     if not ingredients:
         url = ('https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&random=true&mealType=Dinner'
                .format(api_id, api_key))
+    # if ingredients are inputted, these are passed to the API
     else:
+        # inputted ingredients joined in a list
         formatted_user_ingredients = ",".join(ingredients)
         url = ('https://api.edamam.com/api/recipes/v2?type=public&q={}&app_id={}&app_key=%20{}&random=true&mealType='
                'Dinner').format(formatted_user_ingredients, api_id, api_key)
-    print(url)  # TODO delete at end
     response = requests.get(url)
     data = response.json()
     hits = data['hits']
+    # if no recipes are found for the inputted ingredient, None is returned
     if not hits:
         return None
+    # if a recipe is found for the inputted ingredients, the first recipe is returned
     else:
         single_recipe = data['hits'][0]['recipe']
         return single_recipe
 
 
-def ingredients_is_valid(ingredients):
-    return True
-
-
+# method for asking user for ingredient inputs
 def ask_ingredients():
     user_ingredients = []
     first_ingredients_input = input("Please enter your first ingredient. Leave blank for a randomised recipe: ").strip()
+    # if no ingredient is inputted, an empty list (user_ingredients) is returned
     if first_ingredients_input == '':
         return user_ingredients
+    # if a first ingredient is inputted, the user is asked to input the next ingredient
     else:
+        # user ingredient list appended to add each ingredient inputted by user
         user_ingredients.append(first_ingredients_input)
     next_ingredients_input = input("Please enter your next ingredient. Leave blank and enter once all ingredients"
                                    " inputted: ").strip()
+    # whilst an ingredient is inputted, the user will continue to be asked to input the next ingredient
     while next_ingredients_input != '':
+        # user ingredient list appended to add each ingredient inputted by user
         user_ingredients.append(next_ingredients_input)
         next_ingredients_input = input("Please enter your next ingredient. Leave blank and enter once all ingredients "
                                        "inputted: ").strip()
+    # when the user stops inputting ingredients, return the list of user ingredients
     return user_ingredients
 
 
+# method for writing API recipe title, image, ingredient list, instructions link & API return time to markdown file
 def write_to_file(recipe, elapsed_time):
     recipe_title = recipe['label']
+    # first 10 letters of recipe title used as markdown file name
     file_name = recipe_title[0:10].replace(" ", "_")
     image = recipe['image']
     recipe_ingredients = recipe['ingredientLines']
     url = recipe['url']
     recipe_instructions_url = ("See full cooking instructions on: {}".format(url))
     timer = ("It took us {}ms to find a recipe for your dinner tonight!".format(elapsed_time))
+    # ingredients in list formatted into bullet points in markdown
     formatted_ingredients = "\n".join(["- {}".format(item) for item in recipe_ingredients])
     with open('recipe_output/{}.md'.format(file_name), 'w') as file:
         markdown_content = ('# {} \n\n'
@@ -65,20 +75,23 @@ def write_to_file(recipe, elapsed_time):
                             '## Instructions:\n{} \n\n'
                             '### Powered by Edamam \n{}'
                             .format(recipe_title, image, formatted_ingredients, recipe_instructions_url, timer))
+        # all formatted API recipe content written to markdown file
         file.writelines(markdown_content)
 
 
+# main method, calling on all above supplementary methods
 def main():
     print("Hello! Allow Edamam's API to decide what's for dinner! Tell us what's in your fridge, and we'll provide the"
           " recipe. Bon apetite!")
     ingredients = ask_ingredients()
-    print(ingredients)  # TODO delete at end
     time_now = datetime.now()
     recipe = get_recipe(ingredients, API_ID, API_KEY)
-    pprint.pprint(recipe)  # TODO delete at end
+    # time taken for API to return recipe
     elapsed_time = (datetime.now() - time_now).microseconds/1000
+    # if no recipes are found for inputted ingredients (as per get_recipe method), user is asked to restart app
     if recipe is None:
         print("No recipes found for your entered ingredients. Please restart app and try again.")
+    # if recipes are found for inputted ingredients, write them to file
     else:
         write_to_file(recipe, elapsed_time)
 
