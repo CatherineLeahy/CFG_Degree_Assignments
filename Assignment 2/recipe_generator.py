f88a77f55ca941e920d00b619b6516be2859484f
Catherine Leahy <catleahy9@msn.com>
2024-06-02 19:41:47 +0100

    ingredients list formatted, output moved to gitignore

--- Assignment 2/recipe_generator.py
+++ Assignment 2/recipe_generator.py
@@ -4,12 +4,12 @@ import pprint
 import requests
 
 
-# My API access ID and key
+# My API access ID and key, see README for instructions on acquiring your own ID and key.
 API_ID = 'ae7b3fe0'
 API_KEY = '9df3c1657ee8917acb0978d30b1af0e6'
 
 
-#
+# Method for
 def get_recipe(ingredients, api_id, api_key):
     if not ingredients:
         url = ('https://api.edamam.com/api/recipes/v2?type=public&app_id={}&app_key={}&random=true&mealType=Dinner'
@@ -18,11 +18,15 @@ def get_recipe(ingredients, api_id, api_key):
         formatted_user_ingredients = ",".join(ingredients)
         url = ('https://api.edamam.com/api/recipes/v2?type=public&q={}&app_id={}&app_key=%20{}&random=true&mealType='
                'Dinner').format(formatted_user_ingredients, api_id, api_key)
-    print(url)
+    print(url)  # TODO delete at end
     response = requests.get(url)
     data = response.json()
-    single_recipe = data['hits'][0]['recipe']  # add handling for no recipes
-    return single_recipe
+    hits = data['hits']
+    if not hits:
+        return None
+    else:
+        single_recipe = data['hits'][0]['recipe']
+        return single_recipe
 
 
 def ingredients_is_valid(ingredients):
@@ -47,28 +51,36 @@ def ask_ingredients():
 
 def write_to_file(recipe, elapsed_time):
     recipe_title = recipe['label']
+    file_name = recipe_title[0:10].replace(" ", "_")
     image = recipe['image']
     recipe_ingredients = recipe['ingredientLines']
     url = recipe['url']
     recipe_instructions_url = ("See full cooking instructions on: {}".format(url))
     timer = ("It took us {}ms to find a recipe for your dinner tonight!".format(elapsed_time))
-    with open('recipe_output.md', 'w') as file:
-        file.writelines('# {} \n\n![recipe_image]({}) \n\n{} \n\n{} \n\n{}'.format(recipe_title, image,
-                                                                                   recipe_ingredients,
-                                                                                   recipe_instructions_url, timer))
-
+    formatted_ingredients = "\n".join(["- {}".format(item) for item in recipe_ingredients])
+    with open('recipe_output/{}.md'.format(file_name), 'w') as file:
+        markdown_content = ('# {} \n\n'
+                            '![recipe_image]({}) \n\n'
+                            '## Ingredients:\n{} \n\n'
+                            '## Instructions:\n{} \n\n'
+                            '### Powered by Edamam \n{}'
+                            .format(recipe_title, image, formatted_ingredients, recipe_instructions_url, timer))
+        file.writelines(markdown_content)
 
 
 def main():
     print("Hello! Allow Edamam's API to decide what's for dinner! Tell us what's in your fridge, and we'll provide the"
-          " recipe. Bon apetite!.")
+          " recipe. Bon apetite!")
     ingredients = ask_ingredients()
-    print(ingredients)  # delete at end
+    print(ingredients)  # TODO delete at end
     time_now = datetime.now()
     recipe = get_recipe(ingredients, API_ID, API_KEY)
-    pprint.pprint(recipe)  # delete at end
+    pprint.pprint(recipe)  # TODO delete at end
     elapsed_time = (datetime.now() - time_now).microseconds/1000
-    write_to_file(recipe, elapsed_time)
+    if recipe is None:
+        print("No recipes found for your entered ingredients. Please restart app and try again.")
+    else:
+        write_to_file(recipe, elapsed_time)
 
 
 if __name__ == "__main__":
